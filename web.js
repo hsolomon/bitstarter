204503e3b5526addd9ca67acc2798d7873153905
Haydn Solomon <haydn.solomon@gmail.com>
2013-07-08 02:41:39 +0000

    Display message from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buf = new Buffer(30);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = fs.readFileSync('index.html');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
